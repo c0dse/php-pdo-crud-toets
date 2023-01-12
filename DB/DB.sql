ad40f224e173a90be34173a9e598b8fb970dbe67
c0dse <61627362+jasperd033@users.noreply.github.com>
2023-01-12 13:41:35 +0100

    sql

---
+++ DB/DB.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS `php-pdo-crud-toets`;
+USE `php-pdo-crud-toets`;
+
+DROP TABLE IF EXISTS `DureAuto`;
+CREATE TABLE IF NOT EXISTS `DureAuto` (
+  `Id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `Merk` varchar(200) NOT NULL,
+  `Model` varchar(200) NOT NULL,
+  `Topsnelheid` smallint(4) NOT NULL,
+  `Prijs` varchar(200) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+INSERT INTO `DureAUto` (`Id`, `Merk`, `Model`, `Topsnelheid`, `Prijs`) VALUES
+(1, `Bugatti`, `La voiture noire`, 420, ``),
+(2, `Rolls-Royce`, `Swaptail`, 250, ``),
+(3, `Bugatti`, `EB110`, 390, ``),
+(4, `Mercedes-Benz`, `Maybach Exelero`, ``, ``),
+(5, `Koenigsegg`, `CCXR Trevita`, ``, ``);
